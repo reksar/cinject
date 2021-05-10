a1ae55eb0e5a1c753b179f37720ca6e8e24d36ae
reksarka <reksarka@gmail.com>
2021-05-10 16:52:35 +0300

    Uses BYTE* as process base instead of address value.

--- injector.cpp
+++ injector.cpp
@@ -7,16 +7,16 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 
-DWORD get_PID(const char *PrName)
+DWORD getPID(const CHAR *PrName)
 {
-  PROCESSENTRY32 entry;
-  entry.dwSize = sizeof(PROCESSENTRY32);
+  PROCESSENTRY32W entry;
+  entry.dwSize = sizeof(PROCESSENTRY32W);
   HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-  if (Process32First(snapshot, &entry) == TRUE)
+  if (Process32First(snapshot, (LPPROCESSENTRY32)&entry) == TRUE)
   {
-    while (Process32Next(snapshot, &entry) == TRUE)
+    while (Process32Next(snapshot, (LPPROCESSENTRY32)&entry) == TRUE)
     {
-      if (strcmp((char *)entry.szExeFile, PrName) == 0)
+      if (strcmp((CHAR *)entry.szExeFile, PrName) == 0)
       {
         CloseHandle(snapshot);
         return entry.th32ProcessID;
@@ -27,39 +27,39 @@ DWORD get_PID(const char *PrName)
   return NULL;
 }
 
-DWORD GetModuleBase(const char *lpModuleName, DWORD dwProcessId)
+BYTE *GetModuleBase(const CHAR *lpModuleName, DWORD dwProcessId)
 {
-  MODULEENTRY32 lpModuleEntry = { 0 };
-  HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
-
-  if (!hSnapShot)
+  MODULEENTRY32W lpModuleEntry = { 0 };
+  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
+  if (! snapshot)
     return NULL;
   
   lpModuleEntry.dwSize = sizeof(lpModuleEntry);
-  BOOL bModule = Module32First(hSnapShot, &lpModuleEntry);
+  BOOL bModule = Module32First(snapshot, (LPMODULEENTRY32)&lpModuleEntry);
   while (bModule)
   {
-    if (!strcmp((char *)lpModuleEntry.szModule, lpModuleName))
+    if (!strcmp((CHAR *)lpModuleEntry.szModule, lpModuleName))
     {
-      CloseHandle(hSnapShot);
-      return (DWORD)lpModuleEntry.modBaseAddr;
+      CloseHandle(snapshot);
+      return lpModuleEntry.modBaseAddr;
     }
-    bModule = Module32Next(hSnapShot, &lpModuleEntry);
+    bModule = Module32Next(snapshot, (LPMODULEENTRY32)&lpModuleEntry);
   }
-  CloseHandle(hSnapShot);
+  CloseHandle(snapshot);
   return NULL;
 }
 
-int main()
+INT main()
 {
-  const char *PrName = "test.exe";
+  const CHAR *PrName = "test.exe";
+
   DWORD PID;
-  if (!(PID = get_PID(PrName)))
+  if (!(PID = getPID(PrName)))
   {
     printf("Process not found: %s\n", PrName);
     return 1;
   }
-  printf("Process %s has PID %d\n", PrName, PID);
+  printf("Process %s\nPID: %d\n", PrName, PID);
 
   HANDLE hProcess;
   if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID)))
@@ -69,13 +69,25 @@ int main()
   }
   printf("Handle: %d\n", hProcess);
 
-  DWORD BaseAddress;
-  if (!(BaseAddress = GetModuleBase(PrName, PID)))
+  BYTE *pBase;
+  if (!(pBase = GetModuleBase(PrName, PID)))
   {
     printf("Can not get BaseAddress of PID %d\n", PID);
     return 3;
   }
-  printf("BaseAddress: %x\n", BaseAddress);
+
+  BYTE *pBuffer = pBase + 0x0;
+  BYTE *pfuncPrintMessage = pBase + 0x0;
+
+  const CHAR messageLength = 16; // "default message"
+  CHAR localBuffer[messageLength];
+  ReadProcessMemory(
+    hProcess,
+    (void *)pBuffer,
+    &localBuffer,
+    sizeof(CHAR) * messageLength,
+    0);
+  printf("Buffer: %s\n", localBuffer);
   
   return 0;
 }
