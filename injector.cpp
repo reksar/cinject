8ec4ccbace66b81d2ca8f410412c69596be4101c
reksarka <reksarka@gmail.com>
2021-05-15 15:51:44 +0300

    Separates shellcode constants into header file.

--- injector.cpp
+++ injector.cpp
@@ -7,13 +7,7 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 
-const CHAR SHELLCODE[] = "\x48\xB8\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\x48\x89\xC1\x48\xB8\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\x48\x89\xC2\x48\xB8\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xFF\xD0\xC3";
-const DWORD64 FMT_ADDRESS_OFFSET = 2;
-const DWORD64 MSG_ADDRESS_OFFSET = 15;
-const DWORD64 CALL_ADDRESS_OFFSET = 28;
-const SIZE_T SZ_MESSAGE_MAX = 260;
-const SIZE_T SZ_SHELLCODE = sizeof(SHELLCODE);
-const SIZE_T SZ_MEMORY = SZ_SHELLCODE + SZ_MESSAGE_MAX;
+#include "shellcode.h"
 
 struct ProcessEntry
 {
