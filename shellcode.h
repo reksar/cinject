fccc7726d0ebd3e1122e278196e96213d3220952
reksarka <reksarka@gmail.com>
2021-05-16 10:00:31 +0300

    Uses translated obj -> h shellcode.

--- shellcode.h
+++ shellcode.h
@@ -4,6 +4,8 @@
 #include <minwindef.h>
 
 /*
+ * The shellcode is:
+ *
  * mov rax, 0xaaaaaaaaaaaaaaaa ; "%s" fmt
  * mov rcx, rax
  * mov rax, 0xbbbbbbbbbbbbbbbb ; msg
@@ -11,17 +13,27 @@
  * mov rax, 0xcccccccccccccccc ; printf()
  * call rax
  * ret
+ * 
+ * I compiled it with NASM, deleted all additional code and put 
+ * build/shellcode.obj file with same content as below:
+ * 
+ * \x48\xB8\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\x48\x89\xC1\x48\xB8
+ * \xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\x48\x89\xC2\x48\xB8
+ * \xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xFF\xD0\xC3
+ * 
+ * xxd -i shellcode.obj > shellcode.obj.h
  */
-const CHAR SHELLCODE[] =
-  "\x48\xB8\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\x48\x89\xC1\x48\xB8"
-  "\xBB\xBB\xBB\xBB\xBB\xBB\xBB\xBB\x48\x89\xC2\x48\xB8"
-  "\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xFF\xD0\xC3";
+#include "shellcode.obj.h"
+extern unsigned char shellcode_obj[];
+extern unsigned int shellcode_obj_len;
+
+const BYTE* SHELLCODE = shellcode_obj;
+const SIZE_T SZ_SHELLCODE = shellcode_obj_len;
 
 const DWORD64 FMT_ADDRESS_OFFSET = 2;
 const DWORD64 MSG_ADDRESS_OFFSET = 15;
 const DWORD64 CALL_ADDRESS_OFFSET = 28;
 const SIZE_T SZ_MESSAGE_MAX = 260;
-const SIZE_T SZ_SHELLCODE = sizeof(SHELLCODE);
 const SIZE_T SZ_MEMORY = SZ_SHELLCODE + SZ_MESSAGE_MAX;
 
 #endif  // CINJECT_SHELLCODE_H_
