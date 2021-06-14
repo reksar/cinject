032d389ffa4e193d2c8d43ec8fc4101145d54e2f
reksarka <reksarka@gmail.com>
2021-06-14 13:25:32 +0300

    Uses the vscmd submodule to init the C++ compiler.

--- utils/compile.bat
+++ utils/compile.bat
@@ -1,2 +1,3 @@
-@CALL utils\vcvars.bat || EXIT
+@CALL utils\config.bat || EXIT
+@CALL utils\vscmd\init.bat || EXIT
 cl /MD /W4 /EHsc /std:c++17 /O2 /Fo%OUTDIR%\\ /Fe%OUTDIR%\\ %1
--- utils/config.bat
+++ utils/config.bat
@@ -7,12 +7,6 @@ SET NASM_PATH=C:\path\to\nasm
 
 REM --- Optional --------------------------------------------------------------
 
-REM VS build tools may be installed to a non-default path, so we set the link
-REM at the Start menu instead of path to a vcvars bat-file.
-SET VCVARS_LNK=%ProgramData%\Microsoft\Windows\Start Menu\Programs
-SET VCVARS_LNK=%VCVARS_LNK%\Visual Studio 2019\Visual Studio Tools\VC
-SET VCVARS_LNK=%VCVARS_LNK%\x64 Native Tools Command Prompt for VS 2019.lnk
-
 SET OUTDIR=build
 
 REM --- Init ------------------------------------------------------------------
