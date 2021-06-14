cec48f857af1295297f032ddb62ca224e065c6eb
reksarka <reksarka@gmail.com>
2021-06-14 13:12:26 +0300

    Shows the Git and NASM paths errors.

--- utils/config.bat
+++ utils/config.bat
@@ -17,6 +17,16 @@ SET OUTDIR=build
 
 REM --- Init ------------------------------------------------------------------
 
+IF NOT EXIST "%BIN_PATH%" (
+  ECHO Path to Git's usr\bin is invalid: "%BIN_PATH%".
+  EXIT /B 1
+)
+
+IF NOT EXIST "%NASM_PATH%" (
+  ECHO NASM is not found at "%NASM_PATH%".
+  EXIT /B 2
+)
+
 SET PATH=%PATH%;%BIN_PATH%;%NASM_PATH%
 
 IF NOT EXIST "%OUTDIR%" (
